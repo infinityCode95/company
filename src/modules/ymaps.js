f0a6267846cebcbab47975c5a3377232484e3364
Artem Tsykun <tema.2195@yandex.ru>
2023-03-09 12:38:48 +0300

    Changed style in the folder style, change maps.js in the folder modules

--- src/modules/ymaps.js
+++ src/modules/ymaps.js
@@ -1,47 +1,9 @@
+function init() {
+  let map = new ymaps.Map("map", {
+    center: [45.04444314029217,39.029241615063775],
+    zoom: 16,
+  });
+}
+
 ymaps.ready(init);
 
-function init() {
-  var proxYaMap = new ymaps.Map("proxYaMap", {
-    center: [53.340318, 83.693563],
-    zoom: 12,
-  });
-
-  // proxYaMap.geoObjects.add(myGeoObject);
-  proxYaMap.geoObjects
-    .add(
-      new ymaps.Placemark(
-        [53.338532, 83.785333],
-        {
-          iconCaption: "ПРОКС",
-        },
-        {
-          preset: "islands#circleIcon",
-          iconColor: "#072D82",
-        }
-      )
-    )
-    .add(
-      new ymaps.Placemark(
-        [53.338606, 83.66361],
-        {
-          iconCaption: "ПРОКС",
-        },
-        {
-          preset: "islands#circleIcon",
-          iconColor: "#072D82",
-        }
-      )
-    )
-    .add(
-      new ymaps.Placemark(
-        [53.337839, 83.783144],
-        {
-          iconCaption: "ПРОКС",
-        },
-        {
-          preset: "islands#circleIcon",
-          iconColor: "#072D82",
-        }
-      )
-    );
-}
